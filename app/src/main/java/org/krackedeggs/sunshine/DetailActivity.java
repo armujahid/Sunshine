006a0cac3cfafd1a89b5513bee8812a4331ffc2a
Abdul Rauf <armujahid@live.com>
2015-09-25 20:38:49 +0500

    Loader implementation in DetailActivity

--- app/src/main/java/org/krackedeggs/sunshine/DetailActivity.java
+++ app/src/main/java/org/krackedeggs/sunshine/DetailActivity.java
@@ -2,10 +2,15 @@ package org.krackedeggs.sunshine;
 
 //import android.app.Fragment;
 import android.content.Intent;
+import android.database.Cursor;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 //import android.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,6 +22,7 @@ import android.support.v7.widget.ShareActionProvider;
 import android.support.v4.view.MenuItemCompat;
 
 import android.widget.TextView;
+import org.krackedeggs.sunshine.data.WeatherContract.WeatherEntry;
 
 
 public class DetailActivity extends AppCompatActivity {
@@ -27,7 +33,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
     }
@@ -46,31 +52,131 @@ public class DetailActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Intent settingsIntent = new Intent(this,SettingsActivity.class);
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
             startActivity(settingsIntent);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
         private ShareActionProvider mShareActionProvider;
-        Intent sendIntent = new Intent();
-        String mForecastStr="";
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+        String mForecast = "";
+        private static final int FORECAST_DETL_LOADER = 0;
+        private static final String[] FORECAST_COLUMNS = {
+                WeatherEntry.TABLE_NAME + "." + WeatherEntry._ID,
+                WeatherEntry.COLUMN_DATE,
+                WeatherEntry.COLUMN_SHORT_DESC,
+                WeatherEntry.COLUMN_MAX_TEMP,
+                WeatherEntry.COLUMN_MIN_TEMP,
+        };
 
-        public PlaceholderFragment() {
+        // these constants correspond to the projection defined above, and must change if the
+        // projection changes
+        private static final int COL_WEATHER_ID = 0;
+        private static final int COL_WEATHER_DATE = 1;
+        private static final int COL_WEATHER_DESC = 2;
+        private static final int COL_WEATHER_MAX_TEMP = 3;
+        private static final int COL_WEATHER_MIN_TEMP = 4;
+
+
+        public DetailFragment() {
+        }
+
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mForecast + FORECAST_SHARE_HASHTAG);
+            return shareIntent;
         }
 
         @Override
-        public void onCreate(Bundle savedInstanceState){
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            // Prepare the loader.  Either re-connect with an existing one,
+            // or start a new one.
+            getLoaderManager().initLoader(FORECAST_DETL_LOADER, null, this);
+        }
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            Log.v(LOG_TAG, "In onCreateLoader");
+            Intent intent = getActivity().getIntent();
+            if (intent == null) {
+                return null;
+            }
+
+            // Now create and return a CursorLoader that will take care of
+            // creating a Cursor for the data being displayed.
+            return new CursorLoader(
+                    getActivity(),
+                    intent.getData(),
+                    FORECAST_COLUMNS,
+                    null,
+                    null,
+                    null
+            );
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            //raufa: viewgroup is null
+            Log.v(LOG_TAG, "In onLoadFinished");
+            if (!data.moveToFirst()) {
+                return;
+            }
+            mForecast = convertCursorRowToUXFormat(data);
+            TextView detailTextView = (TextView) getView().findViewById(R.id.detail_text);
+            detailTextView.setText(mForecast);
+
+            // If onCreateOptionsMenu has already happened, we need to update the share intent now.
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+
+        }
+
+        //TODO: below two fucntions are copied from ForecastAdapter, remove this duplication
+        /**
+         * Prepare the weather high/lows for presentation.
+         */
+        private String formatHighLows(double high, double low) {
+            boolean isMetric = org.krackedeggs.sunshine.Utility.isMetric(getActivity());
+            String highLowStr = org.krackedeggs.sunshine.Utility.formatTemperature(high, isMetric) + "/" + org.krackedeggs.sunshine.Utility.formatTemperature(low, isMetric);
+            return highLowStr;
+        }
+
+        /*
+            This is ported from FetchWeatherTask --- but now we go straight from the cursor to the
+            string.
+         */
+        private String convertCursorRowToUXFormat(Cursor cursor) {
+            String highAndLow = formatHighLows(
+                    cursor.getDouble(COL_WEATHER_MAX_TEMP),
+                    cursor.getDouble(COL_WEATHER_MIN_TEMP));
+
+            return org.krackedeggs.sunshine.Utility.formatDate(cursor.getLong(COL_WEATHER_DATE)) +
+                    " - " + cursor.getString(COL_WEATHER_DESC) +
+                    " - " + highAndLow;
+        }
+
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setHasOptionsMenu(true); //for sending fragment's menu options to detailactivity
 
@@ -79,34 +185,24 @@ public class DetailActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            Intent intent = getActivity().getIntent();
-            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                mForecastStr = intent.getStringExtra(intent.EXTRA_TEXT);//ForecastFragment.EXTRA_TEXT);
-            }
-            if (null != mForecastStr) {
-                ((TextView) rootView.findViewById(R.id.detail_text)).setText(mForecastStr);
-            }
-            return rootView;
+            return inflater.inflate(R.layout.fragment_detail, container, false);
         }
 
         @Override
-        public void onCreateOptionsMenu(Menu menu,MenuInflater inflater) {
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
             // Inflate the menu; this adds items to the action bar if it is present.
-            inflater.inflate(R.menu.placeholderfragment, menu);
+            inflater.inflate(R.menu.detailfragment, menu);
 
-            // Locate MenuItem with ShareActionProvider
-            MenuItem item = menu.findItem(R.id.menu_item_share);
+            // Retrieve the share menu item
+            MenuItem menuItem = menu.findItem(R.id.menu_item_share);
 
-            // Fetch and store ShareActionProvider
-            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+            // Get the provider and hold onto it to set/change the share intent.
+            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
 
-            sendIntent.setAction(Intent.ACTION_SEND);
-            sendIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr+" #SunShine");
-            sendIntent.setType("text/plain");
-            setShareIntent(sendIntent);
-
-            // Return true to display menu
+            // If onLoadFinished happens before this, we can go ahead and set the share intent now.
+            if (mForecast != null) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            }
         }
 
         @Override
